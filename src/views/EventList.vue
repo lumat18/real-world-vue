d0ad55ae44145a93797d1e30f58f18a59570e7a2
lumat18 <lumat@op.pl>
2020-08-09 23:25:38 +0200

    fetch event details

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -7,7 +7,7 @@
 
 <script>
 import EventCard from "../components/EventCard";
-import axios from "axios";
+import EventService from "../services/EventService";
 
 export default {
   components: { EventCard },
@@ -17,8 +17,7 @@ export default {
     };
   },
   created() {
-    axios
-      .get("http://localhost:3000/events")
+    EventService.getEvents()
       .then(response => {
         this.events = response.data;
       })
