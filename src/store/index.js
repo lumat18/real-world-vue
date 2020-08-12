f5f6ca6e272ce1d1a213132a617386851e2509b2
lumat18 <lumat@op.pl>
2020-08-12 12:12:16 +0200

    add create-event-form and logic behind it

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import EventService from "../services/EventService";
 
 Vue.use(Vuex);
 
@@ -15,12 +16,6 @@ export default new Vuex.Store({
       "food",
       "community"
     ],
-    todos: [
-      { id: 1, text: "...", done: true },
-      { id: 2, text: "...", done: false },
-      { id: 3, text: "...", done: true },
-      { id: 4, text: "...", done: false }
-    ],
     events: [
       { id: 1, title: "...", organizer: "..." },
       { id: 2, title: "...", organizer: "..." },
@@ -30,27 +25,17 @@ export default new Vuex.Store({
     count: 0
   },
   mutations: {
-    INCREMENT_COUNT(state, value) {
-      state.count += value;
+    ADD_EVENT(state, event) {
+      state.events.push(event);
     }
   },
   actions: {
-    updateCount({ state, commit }, value) {
-      if (state.user) {
-        commit("INCREMENT_COUNT", value);
-      }
+    createEvent({ commit }, event) {
+      EventService.postEvent(event);
+      commit("ADD_EVENT", event);
     }
   },
   getters: {
-    catLength: state => {
-      return state.categories.length;
-    },
-    doneTodos: state => {
-      return state.todos.filter(todo => todo.done);
-    },
-    activeTodosCount: (state, getters) => {
-      return state.todos.length - getters.doneTodos.length;
-    },
     getEventById: state => id => {
       return state.events.find(event => event.id === id);
     }
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,49 +1,98 @@
 <template>
   <div>
-    <h1>Create an Event, {{ user.name }}</h1>
-    <p>This event was created by {{ user.id }}</p>
-    <p>There are {{ catLength }} categories:</p>
-    <p>{{ getEventById(1) }}</p>
-    <ul>
-      <li v-for="category in categories" :key="category">{{ category }}</li>
-    </ul>
-    <div>
-      <p>{{ count }}</p>
-      <input type="number" v-model.number="incrementBy" />
-      <button @click="incrementCount">Increment</button>
-    </div>
+    <h1>Create an Event</h1>
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="category in categories" :key="category">
+          {{ category }}
+        </option>
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date" />
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-import { mapGetters } from "vuex";
+import Datepicker from "vuejs-datepicker";
 
 export default {
+  components: {
+    Datepicker
+  },
   data() {
+    const times = [];
+    for (let i = 0; i <= 23; i++) {
+      times.push(i + ":00");
+    }
     return {
-      incrementBy: 1
+      event: this.createNewEvent(),
+      times: times,
+      categories: this.$store.state.categories
     };
   },
-  computed: {
-    catLength() {
-      return this.$store.getters.catLength;
-    },
-    ...mapGetters({
-      getEventById: "getEventById"
-    }),
-    ...mapState({
-      user: "user",
-      categories: "categories",
-      count: "count"
-    })
-  },
   methods: {
-    incrementCount() {
-      this.$store.dispatch("updateCount", this.incrementBy);
+    createNewEvent() {
+      const user = this.$store.state.user;
+      const id = Math.floor(Math.random() * 1000000);
+      return {
+        id: id,
+        organizer: user,
+        category: "",
+        title: "",
+        description: "",
+        location: "",
+        date: "",
+        time: "",
+        attendees: []
+      };
+    },
+    createEvent() {
+      this.$store.dispatch("createEvent", this.event);
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
+</style>
