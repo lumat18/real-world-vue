ce5c8bb876769052da2dddf70ea288ff03400aed
lumat <lukasz.matuszewski@spartez-software.com>
2020-08-12 10:59:46 +0200

    add sample mutations

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,10 +1,48 @@
 <template>
-  <h1>Create an Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories:</p>
+    <p>{{ getEventById(1) }}</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
+    <div>
+      <p>{{ count }}</p>
+      <input type="number" v-model.number="incrementBy" />
+      <button @click="incrementCount">Increment</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import { mapGetters } from "vuex";
+
 export default {
-  name: "EventCreate"
+  data() {
+    return {
+      incrementBy: 1
+    };
+  },
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength;
+    },
+    ...mapGetters({
+      getEventById: "getEventById"
+    }),
+    ...mapState({
+      user: "user",
+      categories: "categories",
+      count: "count"
+    })
+  },
+  methods: {
+    incrementCount() {
+      this.$store.commit("INCREMENT_COUNT", this.incrementBy);
+    }
+  }
 };
 </script>
 
